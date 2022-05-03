650c74e7d7cd3910b0803a18cb72534666dcbeba
Beck Busch <beckbusch5@gmail.com>
2022-05-04 06:20:24 +1200

    Revert "added new main.py for merge"
    This reverts commit 2f84fff4df9297a03e22b4f0c1b249edceaac11a.

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-import time, datetime, math
-import zipfile, wget
-import guiCode
-import threading
-import torchvision
-import numpy as np
-
-from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
 import time, datetime, math
 import zipfile, wget
@@ -27,10 +19,6 @@ progressCheck = 0
 timeTracker = 0
 timeString = ""
 
-last_x = None
-last_y = None
-paintCanvas = None
-
 rowCount = 50
 prev = 0
 
@@ -43,12 +31,12 @@ def train_picture_list():
     ui.tableWidget.setRowCount(math.floor(len(image_array) / 2))
     return image_array
 
-
 def bar_custom(current, total, width=80):
     global progressCheck, timeTracker, timeString
     timeElasped = 0
-
+    
     progress = math.ceil((current/total) * 100)
+
 
     if (progress != progressCheck):
         print("calc")
@@ -59,73 +47,13 @@ def bar_custom(current, total, width=80):
         timeTracker = time.time()
         timeString = str(datetime.timedelta(seconds=timeElasped))
 
+
+
     ui.progressBar.setProperty("value", progress)
     ui.timeRemaininCount.setText(timeString)
 
-def emnistDownload():
-    global progressCheck
-
-    #ui.cancelButton.setEnabled(True)
-    progressCheck = 0
-    wget.download(urlLink, bar=bar_custom)
-    
-    ui.timeRemainingLabel.setText("Unzipping:")
-    zf = zipfile.ZipFile(destination)
-    uncompress_size = sum((file.file_size for file in zf.infolist()))
-    extracted_size = 0
-    for file in zf.infolist():
-        extracted_size += file.file_size
-        progress = math.ceil((extracted_size/uncompress_size) * 100)
-        ui.progressBar.setProperty("value", progress)
-        zf.extract(file)
-
-def cancelDownload():
-    pass
-
-def clearCanvas():
-    paintCanvas.fill(QtGui.QColor("white"))
-    ui.canvasLabel.setPixmap(paintCanvas)
-
-def painting(e):
-    global last_x, last_y
-
-    if last_x is None:
-        last_x = e.x()
-        last_y = e.y()
-        return
-    
-    painter = QtGui.QPainter(paintCanvas)
-    p = painter.pen()
-    p.setWidth(16)
-    p.setColor(QtGui.QColor("black"))
-    painter.setPen(p)
-    #painter.drawLine(last_x+580, last_y+220, e.x()+580, e.y()+220)
-    painter.drawLine(last_x, last_y, e.x(), e.y())
-    painter.end()
-    ui.canvasLabel.setPixmap(paintCanvas)
-
-    # Update the origin for next time.
-    last_x = e.x()
-    last_y = e.y()
-
-def mouseRel(e):
-    global last_x, last_y
-    last_x = None
-    last_y = None
-
-def saveImage():
-    print("test")
-    channels_count = 4
-    image = paintCanvas.toImage()
-    image.save(r'tmp.png')
-
-def test(e):
-    pass
-    #print("pogdfgf")
-    #print(e)
-    #ui.downloadButton.setText("tstst")
-
 def update_pos():
+
     global prev
     pos = ui.tableWidget.verticalScrollBar().value()
     for i in range(14):
@@ -156,6 +84,32 @@ def update_pos():
 
     prev = pos
 
+def emnistDownload():
+    global progressCheck
+
+    #ui.cancelButton.setEnabled(True)
+
+    progressCheck = 0
+    #print("check")
+    wget.download(urlLink, bar=bar_custom)
+    
+    ui.timeRemainingLabel.setText("Unzipping:")
+    zf = zipfile.ZipFile(destination)
+    uncompress_size = sum((file.file_size for file in zf.infolist()))
+    extracted_size = 0
+    for file in zf.infolist():
+        extracted_size += file.file_size
+        progress = math.ceil((extracted_size/uncompress_size) * 100)
+        ui.progressBar.setProperty("value", progress)
+        zf.extract(file)
+
+def guiEdits():
+    #ui.cancelButton.clicked.connect(test)
+    ui.downloadButton.clicked.connect(emnistDownload)
+    ui.tableWidget.verticalScrollBar().valueChanged.connect(update_pos)
+
+def test():
+    print("no")
 
 if __name__ == "__main__":
     import sys
@@ -163,20 +117,9 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = guiCode.Ui_MainWindow()
     ui.setupUi(MainWindow)
-
-    ui.downloadButton.clicked.connect(emnistDownload)
-    ui.clearCanvaButton.clicked.connect(clearCanvas)
-    ui.submitCanvasButton.clicked.connect(saveImage)
-    ui.tableWidget.verticalScrollBar().valueChanged.connect(update_pos)
-
-    #ui.canvasLabel = QtWidgets.QLabel(ui.prediction)
-    #   ui.canvasLabel.setGeometry(QtCore.QRect(20, 60, 250, 250))
-    paintCanvas = QtGui.QPixmap(250, 250)
-    paintCanvas.fill(QtGui.QColor("white"))
-    ui.canvasLabel.setPixmap(paintCanvas)
-    ui.canvasLabel.mouseMoveEvent = painting
-    ui.canvasLabel.mouseReleaseEvent = mouseRel
-    
+    image_array = train_picture_list()
+    update_pos()
+    guiEdits()
     MainWindow.show()
-    MainWindow.update()
     sys.exit(app.exec_())
+
